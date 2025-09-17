9bfc7a318eeb73615b1702138bf3d94364265afc
Chapiuskys <gabrielmp2709@gmail.com>
2025-09-16 21:42:15 -0300

    fix: add animations lib / adjuste components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+// 1. IMPORTAMOS O NOSSO COMPONENTE DA LISTA DE PRODUTOS
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  // 2. ADICIONAMOS ELE NO ARRAY 'IMPORTS' PARA QUE O HTML O RECONHEÇA
+  imports: [CommonModule, RouterOutlet, ProductListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'santander-desafio';
