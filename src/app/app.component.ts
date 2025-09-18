8a2f14763d74ba99d171f99024db818578de717c
Chapiuskys <gabrielmp2709@gmail.com>
2025-09-18 12:54:56 -0300

    feat: Details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
-// IMPORTAM O  COMPONENTE DA LISTA DE PRODUTOS
+// IMPORTAMOS O  COMPONENTE DA LISTA DE PRODUTOS
 import { ProductListComponent } from './components/product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  // ADICIONA ELE NO ARRAY 'IMPORTS' PARA O HTML O RECONHEce
+  //  ADICIONAMOS ELE NO ARRAY 'IMPORTS' PARA O HTML O RECONHECER
   imports: [CommonModule, RouterOutlet, ProductListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
