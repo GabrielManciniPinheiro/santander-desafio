4714dc9655b62aaecf1dae52b3c9d8c8c7cc5f5f
Chapiuskys <gabrielmp2709@gmail.com>
2025-09-18 12:52:52 -0300

    feat: Adjuste comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
-// 1. IMPORTAMOS O NOSSO COMPONENTE DA LISTA DE PRODUTOS
+// IMPORTAM O  COMPONENTE DA LISTA DE PRODUTOS
 import { ProductListComponent } from './components/product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  // 2. ADICIONAMOS ELE NO ARRAY 'IMPORTS' PARA QUE O HTML O RECONHEÇA
+  // ADICIONA ELE NO ARRAY 'IMPORTS' PARA O HTML O RECONHEce
   imports: [CommonModule, RouterOutlet, ProductListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
